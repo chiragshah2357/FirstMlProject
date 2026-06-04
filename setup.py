4454c9d19ede59bf63599dad5844e88f68e6bb89
AlooPizzza <chirag.shah.2357@gmail.com>
2026-06-04 12:18:01 +0530

    main

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+
+
+setup{
+name='FirstMlProject',
+version='0.0.1',
+author='Chirag',
+author_email='chirag.shah.2357@gmail.com',
+packages=find_packages(),
+install_requires=['pandas','numpy','seaborn'] 
+}
